c8f72d5bc96fc4b19731d3b3b51621a5b480c6d9
Koen van Vliet <8by8mail@gmail.com>
2017-02-20 21:20:44 +0100

    Added some comments

--- start_fceux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-fceux --xscale 2 --yscale 2 --loadlua $1 $2 $3
---
+++ start_ledtris.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Filename: start_fceux.sh
+# Project: NES Ledtris
+# Author: Koen van Vliet
+# 
+# Startup script for fceux emulator
+# usage:
+#   ./start_ledtris.sh tetris.nes
+#
+
+fceux --xscale 2 --yscale 2 --loadlua ledtris.lua $1 $2 $3 $4 $5 $6
