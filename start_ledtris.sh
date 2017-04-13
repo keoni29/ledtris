aeacd3f6d71218f1284f7f4df0667098a97c30a0
Koen van Vliet <8by8mail@gmail.com>
2017-04-13 19:41:41 +0200

    Ported changes in avr code to arduino

--- start_ledtris.sh
+++ start_ledtris.sh
@@ -8,4 +8,4 @@
 #   ./start_ledtris.sh tetris.nes
 #
 
-fceux --xscale 2 --yscale 2 --loadlua ledtris.lua $1 $2 $3 $4 $5 $6
+fceux --xscale 2 --yscale 2 --loadlua ledtris.lua tetris.nes $1 $2 $3 $4 $5 $6
